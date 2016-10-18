35243f3161114938fa65966581d96fb8b76b3c67
Keith <hemes1314@163.com>
2016-10-18 14:48:20 +0800

    freemarker demo

---
+++ src/main/java/org/keith/demo/freemarker/HelloFreeMarker.java
@@ -0,0 +1,42 @@
+package org.keith.demo.freemarker;
+
+import java.util.*;
+import java.io.*;
+import freemarker.template.*;
+
+public class HelloFreeMarker {
+
+	private Configuration cfg;
+
+	public void init() throws Exception {
+		// 初始化FreeMarker配置
+		// 创建一个Configuration实例
+		cfg = new Configuration();
+		// 设置FreeMarker的模版文件位置
+		File file = new File("templates");
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		cfg.setDirectoryForTemplateLoading(file);
+	}
+
+	public void process() throws Exception {
+		Map root = new HashMap();
+		root.put("name", "FreeMarker!");
+		root.put("msg", "您已经完成了第一个FreeMarker的示例");
+		Template t = cfg.getTemplate("test.html");
+		t.process(root, new OutputStreamWriter(System.out));
+		
+		//String pagename=("/goods-"+goodsid+".html");
+		//newrequest.setServletPath(pagename);
+		//ThreadContextHolder.setHttpRequest(newrequest);
+		
+		//t.process(mapData, ThreadContextHolder.getHttpResponse().getWriter());
+	}
+
+	public static void main(String[] args) throws Exception {
+		HelloFreeMarker hf = new HelloFreeMarker();
+		hf.init();
+		hf.process();
+	}
+}
